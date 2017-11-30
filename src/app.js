63d3ac7cfb7460100a8b7e205ad700b5761598b3
jicemoon <jicemoon@outlook.com>
2017-11-30 11:14:04 +0800

    ....

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 require("./app.scss");
 
-console.log("Hello from App.js and webpack-dev-server");
-let aa = () => {
-    return "test";
-}
-console.log(aa());
+import {
+  Observable
+} from 'rxjs/observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/operator/scan';
+
+let rxFromEle = document.getElementById('rxFrom');
+let rxFromOutput = document.getElementById('rxFromOutput');
+let obser = Observable.fromEvent(rxFromEle, 'click');
+obser.throttleTime(1000)
+  .scan(count => count + 1, rxFromOutput.innerText - 0)
+  .subscribe((count) => {
+    rxFromOutput.innerText = count;
+  })
